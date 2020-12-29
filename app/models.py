b12c8ffd87d21d574da14a72cbd8a3ed81e78d57
jordiprats <jordi.prats@gmail.com>
2020-12-29 19:05:59 +0100

    debug minio s3

--- app/models.py
+++ app/models.py
@@ -22,13 +22,18 @@ def init_s3_client():
     global s3_client
 
     if not s3_client:
-        s3_client = boto3.client(
-                                    service_name='s3',
-                                    endpoint_url=MINIO_URL,
-                                    aws_access_key_id=MINIO_ACCESS_KEY,
-                                    aws_secret_access_key=MINIO_SECRET_KEY,
-                                    config=Config(signature_version='s3v4'),
-                                )
+        try:
+            print("connecting: "+MINIO_URL)
+            s3_client = boto3.client(
+                                        service_name='s3',
+                                        endpoint_url=MINIO_URL,
+                                        aws_access_key_id=MINIO_ACCESS_KEY,
+                                        aws_secret_access_key=MINIO_SECRET_KEY,
+                                        config=Config(signature_version='s3v4'),
+                                    )
+        except:
+            print("ERROR: unable to connect to bucket")
+            raise Exception('unable to connect to bucket')
 
 class Post:
     url = None
