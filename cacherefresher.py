2fb22c50e82142260b14e2c6e77e912dfaa9d431
Jordi Prats <jordi.prats@gmail.com>
2021-10-16 12:24:33 +0200

    added cache refresher

---
+++ cacherefresher.py
@@ -0,0 +1,15 @@
+import app.models
+
+import requests
+import time
+
+# Posts
+
+posts = app.models.Post.all(page=0, limit=-1)['Posts']
+
+while True:
+    for post in posts:
+        print("==="+post.url)
+        requests.get("https://pet2cattle.com"+post.url)
+        time.sleep(60)
+    time.sleep(600)
