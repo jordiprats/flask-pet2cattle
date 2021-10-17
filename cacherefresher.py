3e319a1249f1e13b7abea3bd6e12e7bfa1b84cf9
Jordi Prats <jordi.prats@gmail.com>
2021-10-17 20:00:15 +0200

    fix refresher bug

--- cacherefresher.py
+++ cacherefresher.py
@@ -5,11 +5,12 @@ import time
 
 # Posts
 
-posts = app.models.Post.all(page=0, limit=-1)['Posts']
-
 while True:
-    for post in posts:
-        print("==="+post.url)
-        requests.get("https://pet2cattle.com"+post.url)
-        time.sleep(60)
-    time.sleep(600)
+  posts = app.models.Post.all(page=0, limit=-1)['Posts']
+
+  for post in posts.reverse():
+    print("==="+post.url)
+    requests.get("https://pet2cattle.com"+post.url)
+    time.sleep(30)
+  
+  time.sleep(600)
