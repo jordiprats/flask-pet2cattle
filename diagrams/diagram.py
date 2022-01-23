b48714500a6cd650c1dcf6aa093207bfdeb91de0
Jordi Prats <jordi.prats@gmail.com>
2022-01-23 17:15:21 +0100

    mes detalls

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -5,6 +5,7 @@ from diagrams.k8s.compute import Pod
 from diagrams.k8s.storage import Vol
 from diagrams.onprem.network import Traefik
 from diagrams.onprem.vcs import Github
+from diagrams.k8s.network import Ing, SVC
 
 with Diagram("pet2cattle", show=False):
   ingress_controller = Traefik("ingress controller")
@@ -23,9 +24,19 @@ with Diagram("pet2cattle", show=False):
     git_repo >> pet2cattle_s3sync
 
     pet2cattle_s3sync >> s3_bucket
+
+    blog_ingress = Ing("blog content")
+    static_ingress = Ing("static content")
+
+    svc_blog = SVC("blog")
+    svc_static = SVC("static")
+
+    blog_ingress >> svc_blog >> pet2cattle_blog
+    static_ingress >> svc_static >> pet2cattle_static
+
   
-  ingress_controller >> pet2cattle_blog
-  ingress_controller >> pet2cattle_static
+  ingress_controller >> blog_ingress
+  ingress_controller >> static_ingress
 
 
     
