fa8c8de33368dbc961299f3f5250da6e2d9a8111
Jordi Prats <jordi.prats@gmail.com>
2022-05-29 18:51:51 +0200

    conditional s3 sync

--- sync.sh
+++ sync.sh
@@ -38,17 +38,28 @@ then
     rclone mkdir "minio:${MINIO_BUCKET}"
 fi
 
+PREV_HASH=""
+
 while true;
 do
     rm -fr repo
     mkdir repo
     git clone "${POSTS_REPO}" repo
 
-    rclone --no-update-modtime sync repo/favicon.ico "minio:${MINIO_BUCKET}"
-    rclone --no-update-modtime sync repo/posts       "minio:${MINIO_BUCKET}/posts"
-    rclone --no-update-modtime sync repo/pages       "minio:${MINIO_BUCKET}/pages"
-    rclone --no-update-modtime sync repo/static      "minio:${MINIO_BUCKET}/static"
-    rclone --no-update-modtime sync repo/redirects   "minio:${MINIO_BUCKET}/redirects"
+    cd repo
+    CURRENT_HASH=$(git rev-parse HEAD)
+    cd ..
+
+    if [ "${CURRENT_HASH}" != "${PREV_HASH}"];
+    then
+        rclone --no-update-modtime sync repo/favicon.ico "minio:${MINIO_BUCKET}"
+        rclone --no-update-modtime sync repo/posts       "minio:${MINIO_BUCKET}/posts"
+        rclone --no-update-modtime sync repo/pages       "minio:${MINIO_BUCKET}/pages"
+        rclone --no-update-modtime sync repo/static      "minio:${MINIO_BUCKET}/static"
+        rclone --no-update-modtime sync repo/redirects   "minio:${MINIO_BUCKET}/redirects"
+
+        PREV_HASH="${CURRENT_HASH}"
+    fi
 
     # random sleep between 30 an 39 minutes
     sleep "$(echo "$(echo $RANDOM | grep -Eo "[0-9]$")+30" | bc -l)"m
