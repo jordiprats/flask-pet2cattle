92e72bfd01f855c01b6d0e3c76f491fbc6bba735
Jordi Prats <jordi.prats@gmail.com>
2023-05-22 18:26:11 +0200

    update diagram with AWS setup

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -6,37 +6,50 @@ from diagrams.k8s.storage import Vol
 from diagrams.onprem.network import Traefik
 from diagrams.onprem.vcs import Github
 from diagrams.k8s.network import Ing, SVC
+from diagrams.generic.device import Tablet
 
 with Diagram("pet2cattle", show=False):
-  ingress_controller = Traefik("ingress controller")
   git_repo = Github("posts")
 
-  s3_bucket = SimpleStorageServiceS3Bucket("S3 bucket")
+  user = Tablet("user")
 
-  with Cluster("pet2cattle namespace"):
-    pet2cattle_blog = Pod("blog")
-    pet2cattle_blog - [Vol("temporal data")]
-    s3_bucket >> pet2cattle_blog
+  with Cluster("AWS"):
 
-    pet2cattle_static = Pod("static content")
-    pet2cattle_s3sync = Pod("s3sync")
+    s3_bucket = SimpleStorageServiceS3Bucket("S3 bucket\nk3s backups")
 
-    git_repo >> pet2cattle_s3sync
+    with Cluster("Autoscaling Group"):
 
-    pet2cattle_s3sync >> s3_bucket
+      with Cluster("EC2 instance"):
+        ingress_controller = Traefik("ingress controller")
 
-    blog_ingress = Ing("blog content")
-    static_ingress = Ing("static content")
+        user >> ingress_controller
 
-    svc_blog = SVC("blog")
-    svc_static = SVC("static")
+        with Cluster("pet2cattle namespace"):
+          minio_bucket = SimpleStorageServiceS3Bucket("minIO")
+          pet2cattle_blog = Pod("blog")
+          pet2cattle_blog - [Vol("temporal data")]
+          minio_bucket >> pet2cattle_blog
 
-    blog_ingress >> svc_blog >> pet2cattle_blog
-    static_ingress >> svc_static >> pet2cattle_static
 
-  
-  ingress_controller >> blog_ingress
-  ingress_controller >> static_ingress
+          pet2cattle_static = Pod("static content")
+          pet2cattle_s3sync = Pod("s3sync")
+
+          git_repo >> pet2cattle_s3sync
+
+          pet2cattle_s3sync >> minio_bucket
+
+          blog_ingress = Ing("blog content")
+          static_ingress = Ing("static content")
+
+          svc_blog = SVC("blog")
+          svc_static = SVC("static")
+
+          blog_ingress >> svc_blog >> pet2cattle_blog
+          static_ingress >> svc_static >> pet2cattle_static
+
+        
+        ingress_controller >> blog_ingress
+        ingress_controller >> static_ingress
 
 
     
